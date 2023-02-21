ee172ef6d81a4e814a49c6467c7ac0a1e199606c
Lucas Maciel Schneider <lksbrs@gmail.com>
2023-02-21 16:34:33 -0300

    Circuit breaker e Fallback

--- src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -2,6 +2,7 @@ package br.com.alurafood.pagamentos.controller;
 
 import br.com.alurafood.pagamentos.dto.PagamentoDto;
 import br.com.alurafood.pagamentos.service.PagamentoService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,5 +52,15 @@ public class PagamentoController {
     public ResponseEntity<PagamentoDto> remover(@PathVariable @NotNull Long id) {
         service.excluirPagamento(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/confirmar")
+    @CircuitBreaker(name= "atualizaPedido", fallbackMethod = "pagamentoAutorizadoComIntegraçãoPendente")
+    public void confirmarPagamento(@PathVariable @NotNull Long id){
+        service.confirmarPagamento(id);
+    }
+
+    public void pagamentoAutorizadoComIntegraçãoPendente(Long id, Exception e){
+        service.alteraStatus(id);
     }
 }
